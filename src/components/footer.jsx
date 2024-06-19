028708ffe5df3a271317edb523d657f81d8ad107
Danny <danny.f.gomez@gmail.com>
2024-06-19 12:26:13 -0400

    import font awesome and link to github, linkedin, and email

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,30 @@
-function Footer () {
-    return (
-        <section className="my-5">
-            icons that link to external sites, linkedIn, github, email
-        </section>
-    );
-}
+import '@fortawesome/fontawesome-free/css/all.css';
 
-export default Footer
+function Footer() {
+    const icons = [
+      {
+        name: "fab fa-github",
+        link: "https://github.com/dgomie"
+      },
+      {
+        name: "fab fa-linkedin",
+        link: "https://www.linkedin.com/in/daniel-gomez-37496057/"
+      },
+      {
+        name: "fa-regular fa-envelope",
+        link: "mailto:danny.f.gomez@gmail.com"
+      }
+    ]
+  
+    return (
+      <footer className="flex-row px-1">
+        {icons.map(icon =>
+        (
+          <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer"><i className={icon.name}></i></a>
+        )
+          )}
+      </footer>
+    );
+  }
+  
+  export default Footer;
