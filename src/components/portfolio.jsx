b52a2b8b223e2b3794d5208d8e3ff56fc01973fd
Danny <danny.f.gomez@gmail.com>
2024-06-19 16:10:17 -0400

    add links to portfolio projects

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,7 +1,29 @@
 function Portfolio () {
     return (
         <section className="my-5">
-           list of github projects/deployed sites
+           <ul>
+                <li>
+                    <a href="https://github.com/dgomie/good-picks">GoodPicks</a>
+                </li>
+                <li>
+                    <a href="https://github.com/dgomie/tech-blog">Tech Blog</a>
+                </li>
+                <li>
+                    <a href="https://github.com/SpencerKlink/Pantry-Raid">Pantry Raid</a>
+                </li>
+                <li>
+                    <a href="https://github.com/dgomie/weather-dashboard">Weather Dashboard</a>
+                </li>
+                <li>
+                    <a href="https://github.com/dgomie/password-generator">Password Generator</a>
+                </li>
+                <li>
+                    <a href="https://github.com/dgomie/work-day-calendar">Work-day Calendar</a>
+                </li>
+                <li>
+                    <a href="https://github.com/dgomie/readme-generator">README Generator</a>
+                </li>
+           </ul>
         </section>
     );
 }
