08e030a4b7e6103fd1db67b6fa78574f90f54e90
Danny <danny.f.gomez@gmail.com>
2024-06-19 16:01:29 -0400

    add contact form to contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,7 +1,16 @@
 function Contact () {
     return (
         <section className="my-5">
-            Contact
+            <form>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" placeholder="Enter your name" />
+                {/* Add more inputs as needed */}
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" placeholder="Enter your email" />
+                <label htmlFor="message">Message:</label>
+                <input type="textarea" id="message" placeholder="Enter your message" />
+                <button type="submit">Submit</button>
+            </form>
         </section>
     );
 }
