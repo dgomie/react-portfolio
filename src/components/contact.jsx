a931654cfe3ce7781c9fcf28294433d936d86133
Danny <danny.f.gomez@gmail.com>
2024-06-19 17:32:53 -0400

    styled contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,13 +1,13 @@
 function Contact () {
     return (
-        <section className="my-5">
+        <section className="my-5 formContainer">
             <form>
                 <label htmlFor="name">Name:</label>
-                <input type="text" id="name" placeholder="Enter your name" />
+                <input type="text" id="name" placeholder="Ned Flanders" />
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" placeholder="Enter your email" />
+                <input type="email" id="email" placeholder="nedflanders@email.com" />
                 <label htmlFor="message">Message:</label>
-                <input type="textarea" id="message" placeholder="Enter your message" />
+                <textarea id="message" placeholder="Enter your message" />
                 <button type="submit">Submit</button>
             </form>
         </section>
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import About from './components/about';
 import Portfolio from './components/portfolio';
 import Contact from './components/contact';
 import Resume from './components/resume';
-// import './index.css';
+import './index.css';
 
 const router = createBrowserRouter([
   {
