8669e23f4cc47ad6aa8738f9a2b66d89c3771612
Danny <danny.f.gomez@gmail.com>
2024-06-19 21:23:56 -0400

    Added new images, descriptions, and links to portfolio section

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -2,28 +2,34 @@ function Portfolio() {
   return (
     <section className="my-5 p-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <div className="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
+        <a href="https://hidden-depths-01820-f84c3739770d.herokuapp.com/">
           <img className="rounded-t-lg" src="/images/music.jpg" alt="" />
         </a>
         <div className="p-5">
-          <a href="#">
+          <a href="https://hidden-depths-01820-f84c3739770d.herokuapp.com/">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               GoodPicks
             </h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+          A social media site designed to help users manage their favorite songs, albums, and artists by providing a user-friendly interface to organize and retrieve music-related data.
+          </p>
+          <p>
+          <i className="fa-brands fa-html5 mr-2"></i>
+          <i className="fa-brands fa-css3-alt mr-2"></i>
+          <i className="fa-brands fa-square-js mr-2"></i>
+          <i className="fa-brands fa-node-js mr-2"></i>
+          <i className="fa-solid fa-database"></i>
           </p>
           <a
-            href="#"
+            href="https://hidden-depths-01820-f84c3739770d.herokuapp.com/"
             className="inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Visit Site
           </a>
 
           <a
-            href="#"
+            href="https://github.com/dgomie/good-picks"
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <span className="fab fa-github mr-1"></span>
@@ -33,28 +39,32 @@ function Portfolio() {
       </div>
 
       <div className="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
+        <a href="https://spencerklink.github.io/Pantry-Raid/">
           <img className="rounded-t-lg" src="/images/grocery.jpg" alt="" />
         </a>
         <div className="p-5">
-          <a href="#">
+          <a href="https://spencerklink.github.io/Pantry-Raid/">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               Pantry Raid
             </h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+          A mobile-first web application designed to help users find recipes based on ingredients they have at hand and view detailed nutritional information for each recipe.
+          </p>
+          <p>
+          <i className="fa-brands fa-html5 mr-2"></i>
+          <i className="fa-brands fa-css3-alt mr-2"></i>
+          <i className="fa-brands fa-square-js mr-2"></i>
           </p>
           <a
-            href="#"
+            href="https://spencerklink.github.io/Pantry-Raid/"
             className="inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Visit Site
           </a>
 
           <a
-            href="#"
+            href="https://github.com/SpencerKlink/Pantry-Raid"
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <span className="fab fa-github mr-1"></span>
@@ -64,28 +74,34 @@ function Portfolio() {
       </div>
 
       <div className="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
+        <a href="https://still-wildwood-68456-d44a68e622ba.herokuapp.com/">
           <img className="rounded-t-lg" src="/images/blog.jpg" alt="" />
         </a>
         <div className="p-5">
-          <a href="#">
+          <a href="https://still-wildwood-68456-d44a68e622ba.herokuapp.com/">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               Tech Blog
             </h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+          A blog site that offers a seamless experience for users to browse existing blog posts, sign up, log in, and manage their own content. 
+          </p>
+          <p>
+          <i className="fa-brands fa-html5 mr-2"></i>
+          <i className="fa-brands fa-css3-alt mr-2"></i>
+          <i className="fa-brands fa-square-js mr-2"></i>
+          <i className="fa-brands fa-node-js mr-2"></i>
+          <i className="fa-solid fa-database"></i>
           </p>
           <a
-            href="#"
+            href="https://still-wildwood-68456-d44a68e622ba.herokuapp.com/"
             className="inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Visit Site
           </a>
 
           <a
-            href="#"
+            href="https://github.com/dgomie/tech-blog"
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <span className="fab fa-github mr-1"></span>
@@ -95,28 +111,32 @@ function Portfolio() {
       </div>
 
       <div className="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
+        <a href="https://dgomie.github.io/weather-dashboard/">
           <img className="rounded-t-lg" src="/images/weather.jpg" alt="" />
         </a>
         <div className="p-5">
-          <a href="#">
+          <a href="https://dgomie.github.io/weather-dashboard/">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               Weather Dashboard
             </h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+            A web application that allows users to search for the current weather conditions of any city by providing real-time and 5-day weather forecasts including temperature, weather description, humidity for the chosen city.
+          </p>
+          <p>
+          <i className="fa-brands fa-html5 mr-2"></i>
+          <i className="fa-brands fa-css3-alt mr-2"></i>
+          <i className="fa-brands fa-square-js mr-2"></i>
           </p>
           <a
-            href="#"
+            href="https://dgomie.github.io/weather-dashboard/"
             className="inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Visit Site
           </a>
 
           <a
-            href="#"
+            href="https://github.com/dgomie/weather-dashboard"
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <span className="fab fa-github mr-1"></span>
@@ -126,28 +146,34 @@ function Portfolio() {
       </div>
 
       <div className="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
+        <a href="https://dgomie.github.io/work-day-calendar/">
           <img className="rounded-t-lg" src="/images/planner.jpg" alt="" />
         </a>
         <div className="p-5">
-          <a href="#">
+          <a href="https://dgomie.github.io/work-day-calendar/">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               Workday Scheduler
             </h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+            A web application that displays an hourly look at a typical 9a-5p workday planner where users can add and edit their schedule. The app provides a visually easy way to determine the current hour in addition to past and future hours.
+
+
+          </p>
+          <p>
+          <i className="fa-brands fa-html5 mr-2"></i>
+          <i className="fa-brands fa-css3-alt mr-2"></i>
+          <i className="fa-brands fa-square-js mr-2"></i>
           </p>
           <a
-            href="#"
+            href="https://dgomie.github.io/work-day-calendar/"
             className="inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Visit Site
           </a>
 
           <a
-            href="#"
+            href="https://github.com/dgomie/work-day-calendar"
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <span className="fab fa-github mr-1"></span>
@@ -157,28 +183,32 @@ function Portfolio() {
       </div>
 
       <div className="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
+        <a href="https://dgomie.github.io/password-generator/">
           <img className="rounded-t-lg" src="/images/password.jpg" alt="" />
         </a>
         <div className="p-5">
-          <a href="#">
+          <a href="https://dgomie.github.io/password-generator/">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               Password Generator
             </h5>
           </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+          A password generator that creates strong passwords using alphanumeric and special characters. Since different sites have varying requirements for password complexity, this generator gives the user the ability to choose the length and types of characters needed.
+          </p>
+          <p>
+          <i className="fa-brands fa-html5 mr-2"></i>
+          <i className="fa-brands fa-css3-alt mr-2"></i>
+          <i className="fa-brands fa-square-js mr-2"></i>
           </p>
           <a
-            href="#"
+            href="https://dgomie.github.io/password-generator/"
             className="inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Visit Site
           </a>
 
           <a
-            href="#"
+            href="https://github.com/dgomie/password-generator"
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <span className="fab fa-github mr-1"></span>
@@ -186,6 +216,42 @@ function Portfolio() {
           </a>
         </div>
       </div>
+
+      <div className="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="https://dgomie.github.io/coding-quiz/">
+          <img className="rounded-t-lg" src="/images/quiz.jpg" alt="" />
+        </a>
+        <div className="p-5">
+          <a href="https://dgomie.github.io/coding-quiz/">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Coding Quiz
+            </h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          A multiple choice coding quiz that features a countdown timer that speeds up on wrong answers. Users are able to save their scores and view a high score list that is local to that computer. 
+          </p>
+          <p>
+          <i className="fa-brands fa-html5 mr-2"></i>
+          <i className="fa-brands fa-css3-alt mr-2"></i>
+          <i className="fa-brands fa-square-js mr-2"></i>
+          </p>
+          <a
+            href="https://dgomie.github.io/coding-quiz/"
+            className="inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Visit Site
+          </a>
+
+          <a
+            href="https://github.com/dgomie/coding-quiz"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <span className="fab fa-github mr-1"></span>
+            Github Repo
+          </a>
+        </div>
+      </div>
+
 
 
     </section>
