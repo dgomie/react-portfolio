57b43e1d4ecaee200c4d93af8283a6b770e23e15
Danny <danny.f.gomez@gmail.com>
2024-06-19 20:43:09 -0400

    switched over to tailwindcss. reformatting portfolio page

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,108 +1,193 @@
 function Portfolio() {
   return (
-    <section className="my-5">
-      <div className="cardContainer">
-        <div className="card">
-          <div className="cardBody">
-            <img
-              src="/images/music.jpg"
-              width="450px"
-              alt="profile image goes here"
-            />
-            <a
-              className="fab fa-github"
-              href="https://github.com/dgomie/good-picks"
-            ></a>
-            <a href="https://hidden-depths-01820-f84c3739770d.herokuapp.com/">
+    <section className="my-5 p-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img className="rounded-t-lg" src="/images/music.jpg" alt="" />
+        </a>
+        <div className="p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               GoodPicks
-            </a>
-          </div>
-        </div>
-        <div className="card">
-          <div className="cardBody">
-            <img
-              src="/images/blog.jpg"
-              width="450px"
-              alt="profile image goes here"
-            />
-            <a
-              className="fab fa-github"
-              href="https://github.com/dgomie/tech-blog"
-            ></a>
-            <a href="https://still-wildwood-68456-d44a68e622ba.herokuapp.com/">
-              Tech Blog
-            </a>
-          </div>
-        </div>
+            </h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Visit Site
+          </a>
 
-        <div className="card">
-          <div className="cardBody">
-            <img
-              src="/images/grocery.jpg"
-              width="450px"
-              alt="profile image goes here"
-            />
-            <a
-              className="fab fa-github"
-              href="https://github.com/SpencerKlink/Pantry-Raid"
-            ></a>
-            <a href="https://spencerklink.github.io/Pantry-Raid/">
-              Pantry Raid
-            </a>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="cardBody">
-            <img
-              src="/images/weather.jpg"
-              width="450px"
-              alt="profile image goes here"
-            />
-            <a
-              className="fab fa-github"
-              href="https://github.com/dgomie/weather-dashboard"
-            ></a>
-            <a href="https://dgomie.github.io/weather-dashboard/">
-              Weather Dashboard
-            </a>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="cardBody">
-            <img
-              src="/images/password.jpg"
-              width="450px"
-              alt="profile image goes here"
-            />
-            <a
-              className="fab fa-github"
-              href="https://github.com/dgomie/password-generator"
-            ></a>
-            <a href="https://github.com/dgomie/password-generator">
-              Password Generator
-            </a>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="cardBody">
-            <img
-              src="/images/planner.jpg"
-              width="450px"
-              alt="profile image goes here"
-            />
-            <a
-              className="fab fa-github"
-              href="https://github.com/dgomie/work-day-calendar"
-            ></a>
-            <a href="https://dgomie.github.io/work-day-calendar/">
-              Workday Scheduler
-            </a>
-          </div>
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <span className="fab fa-github mr-1"></span>
+            Github Repo
+          </a>
         </div>
       </div>
+
+      <div className="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img className="rounded-t-lg" src="/images/grocery.jpg" alt="" />
+        </a>
+        <div className="p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Pantry Raid
+            </h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Visit Site
+          </a>
+
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <span className="fab fa-github mr-1"></span>
+            Github Repo
+          </a>
+        </div>
+      </div>
+
+      <div className="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img className="rounded-t-lg" src="/images/blog.jpg" alt="" />
+        </a>
+        <div className="p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Tech Blog
+            </h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Visit Site
+          </a>
+
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <span className="fab fa-github mr-1"></span>
+            Github Repo
+          </a>
+        </div>
+      </div>
+
+      <div className="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img className="rounded-t-lg" src="/images/weather.jpg" alt="" />
+        </a>
+        <div className="p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Weather Dashboard
+            </h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Visit Site
+          </a>
+
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <span className="fab fa-github mr-1"></span>
+            Github Repo
+          </a>
+        </div>
+      </div>
+
+      <div className="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img className="rounded-t-lg" src="/images/planner.jpg" alt="" />
+        </a>
+        <div className="p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Workday Scheduler
+            </h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Visit Site
+          </a>
+
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <span className="fab fa-github mr-1"></span>
+            Github Repo
+          </a>
+        </div>
+      </div>
+
+      <div className="my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img className="rounded-t-lg" src="/images/password.jpg" alt="" />
+        </a>
+        <div className="p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Password Generator
+            </h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Visit Site
+          </a>
+
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <span className="fab fa-github mr-1"></span>
+            Github Repo
+          </a>
+        </div>
+      </div>
+
+
     </section>
   );
 }
