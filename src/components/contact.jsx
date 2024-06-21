b258acfdad3df61f545ce8e079c22f8b47b70b23
Danny <danny.f.gomez@gmail.com>
2024-06-21 15:58:47 -0400

    adjust form for netlify

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,17 +1,33 @@
-function Contact () {
-    return (
-        <section className="my-5 formContainer">
-            <form className="bg-white" name="contact" method="POST" data-netlify="true">
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name" placeholder="Ned Flanders" />
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" placeholder="nedflanders@email.com" />
-                <label htmlFor="message">Message:</label>
-                <textarea id="message" placeholder="Enter your message" />
-                <button type="submit">Submit</button>
-            </form>
-        </section>
-    );
+function Contact() {
+  return (
+    <section className="my-5 formContainer">
+      <form
+        className="bg-white"
+        name="contact"
+        method="POST"
+        data-netlify="true"
+      >
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message"></textarea>
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+      </form>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
