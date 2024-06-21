76dae959a5d7d5a9a250d8f8dbba906400f1bb3a
Danny <danny.f.gomez@gmail.com>
2024-06-21 15:15:49 -0400

    centered header on smaller screens

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,10 @@
 function Header(props) {
-
   return (
     <header className="mt-2">
-      <h1>Daniel Gomez</h1>
-      <div className="flex justify-center mt-2">
-      {props.children}
+      <div className="text-center m-2">
+        <h1>Daniel Gomez</h1>
       </div>
+      <div className="flex justify-center mt-2">{props.children}</div>
     </header>
   );
 }
