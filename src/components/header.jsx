c3ad25103965dad1ba0e1d5c0b5afbe76de439ea
Danny <danny.f.gomez@gmail.com>
2024-06-21 16:19:07 -0400

    change header name

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ function Header(props) {
   return (
     <header className="mt-2">
       <div className="text-center m-2">
-        <h1>Daniel Gomez</h1>
+        <h1>Danny Gomez</h1>
       </div>
       <div className="flex justify-center mt-2">{props.children}</div>
     </header>
