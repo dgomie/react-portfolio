92b1d2ec046a761c71fcf2365f4c67ac4e91e634
Danny <danny.f.gomez@gmail.com>
2024-06-21 16:05:00 -0400

    revert to formsubmit

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,33 +1,17 @@
-function Contact() {
-  return (
-    <section className="my-5 formContainer">
-      <form
-        className="bg-white"
-        name="contact"
-        method="POST"
-        data-netlify="true"
-      >
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message"></textarea>
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-      </form>
-    </section>
-  );
+function Contact () {
+    return (
+        <section className="my-5 formContainer">
+            <form className="bg-white" action="https://formsubmit.co/4725e9540d3eeede68595f4a2189170d" method="post">
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" placeholder="Ned Flanders" />
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" placeholder="nedflanders@email.com" />
+                <label htmlFor="message">Message:</label>
+                <textarea id="message" placeholder="Enter your message" />
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+    );
 }
 
-export default Contact;
+export default Contact
